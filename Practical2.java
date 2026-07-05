8cd57045861ad7e6617948b6c53aa2ec2cd30e80
ravithakurgit <thakursinghravi786@gmail.com>
2026-07-05 12:29:32 +0530

    Formatted Code

--- Practical2.java
+++ Practical2.java
@@ -89,14 +89,6 @@ public class Practical2 {
                     cipher += encryptBlock(key,
                             text.charAt(i),
                             text.charAt(i + 1));
-                    // int p1 = letterToNumber(text.charAt(i));
-                    // int p2 = letterToNumber(text.charAt(i + 1));
-
-                    // int c1 = (key[0][0] * p1 + key[0][1] * p2) % 26;
-                    // int c2 = (key[1][0] * p1 + key[1][1] * p2) % 26;
-
-                    // cipher += numberToLetter(c1);
-                    // cipher += numberToLetter(c2);
                 }
 
                 System.out.println(cipher);
